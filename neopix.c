6747cac72952118aaad7e19124f58e7115d7438a
Aloysious <aloysiousbenoy@gmail.com>
2023-05-30 03:32:34 +0530

    added blocking tasks

--- neopix.c
+++ neopix.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "pico/stdlib.h"
 
 #include "FreeRTOS.h"
@@ -7,18 +7,39 @@
 
 void vBlinkTask()
 {
-
     vTaskDelay(1000);
-    for (;;)
+    while (true)
     {
         gpio_put(PICO_DEFAULT_LED_PIN, 1);
         vTaskDelay(250);
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
         vTaskDelay(250);
-        sleep_ms(10);
+        printf("Current TASK1 custom count %d\n", getTaskCustomCount());
     }
 }
 
+void vBlinkTask2()
+{
+    while (true)
+    {
+        printf("Current TASK2 custom count %d\n", getTaskCustomCount());
+        // busy wait for 100 ms
+        absolute_time_t timediff = make_timeout_time_ms(100);
+        while (!time_reached(timediff))
+            ;
+    }
+}
+void vBlinkTask3()
+{
+    while (true)
+    {
+        printf("Current TASK3 custom count %d\n ", getTaskCustomCount());
+        // busy wait for 250 ms
+        absolute_time_t timediff = make_timeout_time_ms(250);
+        while (!time_reached(timediff))
+            ;
+    }
+}
 void main()
 {
     stdio_init_all();
@@ -26,7 +47,11 @@ void main()
 
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+    // task with higher priority, this will pre-empt the other two tasks even though they do not yield
+    xTaskCreate(vBlinkTask, "Blink Task", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    // blocking tasks with lower priority
+    xTaskCreate(vBlinkTask2, "Blink Task 2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vBlinkTask3, "Blink Task 3", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     vTaskStartScheduler();
 }
